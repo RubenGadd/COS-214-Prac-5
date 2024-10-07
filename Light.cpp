8b384e271b3d68a36e43fe52654d863c1bebb471
RubenGadd <ruben.gadd@outlook.com>
2024-10-07 19:07:11 +0200

    Minor fixes

--- Light.cpp
+++ Light.cpp
@@ -4,6 +4,10 @@
 /// @brief Construct Light object and passes Light string as device name into Device's constructor
 Light::Light():Device("Light"){}
 
+Light::~Light(){
+	delete sensor;
+}
+
 /// @brief Uses device's getStatus to get the name of the state veriable and returns a string giving the result but in terms of the device, eg Light's are On or Off
 /// @return Returns a string of the devices state
 string Light::getStatus() {
@@ -24,7 +28,7 @@ string Light::getDeviceType() {
 void Light::update() {
 	sensorState = sensor->getState();
 
-	if(sensorState && this->getStatus() == "On") {
+	if(sensorState && this->getStatus() == "Off") {
 		cout << "Motion detected! Turn on the lights";
 		this->performAction(true,"Light");
 		return;
