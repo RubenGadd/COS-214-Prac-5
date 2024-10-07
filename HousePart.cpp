c635cb34ddcb28745a32ba15a127c40aae28f35c
RubenGadd <ruben.gadd@outlook.com>
2024-10-07 09:44:36 +0200

    Added Doxygen to HousePart (Except performAction())

--- HousePart.cpp
+++ HousePart.cpp
@@ -1,5 +1,6 @@
 #include "HousePart.h"
 
+/// @brief Destructs the HousePart Object, which is needed for the list
 Prac_5_Class_Diagram::HousePart::~HousePart(){
 	for( auto * device : children){
 		delete device;
@@ -7,6 +8,8 @@ Prac_5_Class_Diagram::HousePart::~HousePart(){
 	children.clear();
 }
 
+/// @brief A summary is made of the HousePart children list based on the type of device whether the device is active or inactive, eg. number of lights on and off
+/// @return Returns a summary of the number of devices which are activated and Inactive based on the type of device
 string Prac_5_Class_Diagram::HousePart::getStatus() {
 	string out = "Status of Devices in room: \n";
 	int doorLocked = 0;
@@ -47,6 +50,8 @@ string Prac_5_Class_Diagram::HousePart::getStatus() {
 	return out;
 }
 
+/// @brief A summary of the number of devices in the HousePart children list is returned based on the type of device, eg. number of lights
+/// @return Returns a summary of the number of devices
 string Prac_5_Class_Diagram::HousePart::getDeviceType() {
 	string out = "Number of Devices in room: \n";
 	int doorNum = 0;
@@ -77,6 +82,9 @@ void Prac_5_Class_Diagram::HousePart::performAction() {
 	throw "Not yet implemented";
 }
 
+/// @brief It returns the component at the index passed in 
+/// @param index The index to be returned
+/// @return The component located at that given index
 Component * Prac_5_Class_Diagram::HousePart::getChildren(int index) {
 	if(index >= children.size() || index < 0) return nullptr;
 	auto it = children.begin(); 
@@ -84,10 +92,14 @@ Component * Prac_5_Class_Diagram::HousePart::getChildren(int index) {
 	return *it;
 }
 
+/// @brief Add the specified component to the children's list
+/// @param component The component to be added
 void Prac_5_Class_Diagram::HousePart::addChildren(Component* component) {
 	children.push_back(component);
 }
 
+/// @brief Removes the specified component for the children's list
+/// @param component The component to be removed
 void Prac_5_Class_Diagram::HousePart::removeChildren(Component* component) {
 	children.remove(component);
 }
